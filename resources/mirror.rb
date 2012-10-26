92c39339f1c7e28ecd7334ed44ad3da9b64aaa3b
bjoshua <bjoshua@me.com>
2012-10-26 20:23:37 +0000

    Changed to actually check for boolean true or false on cron resource

--- resources/mirror.rb
+++ resources/mirror.rb
@@ -5,7 +5,7 @@ attribute :type, :kind_of => String
 attribute :url, :kind_of => String
 attribute :distribution, :kind_of => String
 attribute :components, :kind_of => Array
-attribute :clean, :kind_of => String
-attribute :cron, :kind_of => Boolean
+attribute :clean, :kind_of => [ TrueCLass, FalseClass ]
+attribute :cron, :kind_of => String
 attribute :schedule, :kind_of => Hash
 attribute :docroot, :kind_of => String, :default => "/var/www/#{:name}"
